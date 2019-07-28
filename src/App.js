9bf8cdf2004a187456919fbe0d4e59a54b27decb
BrainJarProductions <vgrilli1@gmail.com>
2019-07-27 21:10:58 -0400

    add useDebugValue

--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
-import React, { useState, useEffect, useRef, createContext } from 'react';
-import Toggle from './Toggle';
+import React, { useState, useEffect, useRef } from 'react';
 import Counter from './Counter';
 import useTitleInput from './hooks/useTitleInput';
-
-export const UserContext = createContext();
 
 const App = () => {
   const [name, setName] = useTitleInput('');
   const ref = useRef();
-  console.log('ref', ref.current);
 
   return (
-    <UserContext.Provider value={{ user: true }}>
-      <div className="main-wrapper" ref={ref}>
-        <h1>Level Up Dishes</h1>
-        <Toggle />
-        <Counter />
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-          }}
-          action=""
-        >
-          <input
-            type="text"
-            onChange={e => setName(e.target.value)}
-            value={name}
-          />
-          <button>Submit</button>
-        </form>
-      </div>
-    </UserContext.Provider>
+    <div className="main-wrapper" ref={ref}>
+      <h1>Level Up Dishes</h1>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+        }}
+        action=""
+      >
+        <input
+          type="text"
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
   );
 };
 
--- src/hooks/useTitleInput.js
+++ src/hooks/useTitleInput.js
@@ -1,9 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useDebugValue } from 'react';
 
 export default function useTitleInput(initalValue) {
   const [value, setValue] = useState(initalValue);
   useEffect(() => {
     document.title = value;
   });
+  useDebugValue(value);
+
   return [value, setValue];
 }
