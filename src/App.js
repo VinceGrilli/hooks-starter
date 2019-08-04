72c0eb79f36fb3846e170f69571807b178025fe1
BrainJarProductions <vgrilli1@gmail.com>
2019-08-04 19:28:36 -0400

    add useEffet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { useState, useEffect, useRef } from 'react';
-import Counter from './Counter';
 import useTitleInput from './hooks/useTitleInput';
 
 const App = () => {
   const [name, setName] = useTitleInput('');
   const ref = useRef();
+  const [dishes, setDishes] = useState([]);
+
+  const fetchDishes = async () => {
+    const res = await fetch(
+      `https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes`
+    );
+    const data = await res.json();
+    setDishes(data);
+  };
+
+  useEffect(() => {
+    fetchDishes();
+  }, []);
 
   return (
     <div className="main-wrapper" ref={ref}>
@@ -22,6 +34,17 @@ const App = () => {
         />
         <button>Submit</button>
       </form>
+      {dishes.map(dish => (
+        <articale className="dish-card dish-card--withImage">
+          <h3>{dish.name}</h3>
+          <p>{dish.desc}</p>
+          <div className="ingredients">
+            {dish.ingredients.map(ingredients => (
+              <span>{ingredients}</span>
+            ))}
+          </div>
+        </articale>
+      ))}
     </div>
   );
 };
